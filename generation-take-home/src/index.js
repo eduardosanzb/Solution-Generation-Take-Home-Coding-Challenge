3bb42e7ca2e54f2250bb7f1f2e09ab21a63df8e6
eduardosanzb <eduardosanzb@gmail.com>
2017-01-17 23:37:43 -0600

    redux tools

--- generation-take-home/src/index.js
+++ generation-take-home/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import ReduxPromise from 'redux-promise'
 import ReduxThunk from 'redux-thunk' 
 
@@ -9,7 +9,10 @@ import ReduxThunk from 'redux-thunk'
 import App from './components/App';
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise, ReduxThunk)(createStore)
+const createStoreWithMiddleware = compose(
+	applyMiddleware(ReduxPromise, ReduxThunk),
+	window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore)
 
 
 ReactDOM.render(
